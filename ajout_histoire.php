e207bbaa8fd36c086d16252cb987602bc7aba7b9
MarieLeblanc002 <101551141+MarieLeblanc002@users.noreply.github.com>
2022-04-22 11:00:31 +0200

    Travail sur ajout_histoire

--- ajout_histoire.php
+++ ajout_histoire.php
@@ -5,29 +5,15 @@
 <body>
     <div class="container">
         <?php require_once "includes/header.php"; ?>
+        
         <form action="">
-            
-            <table>
-                <tr><td>Informations générales </td></tr>
-                <tr>
-            <td><label for="titre">Titre : </label></td><td><input type="text" name="titre" id="titre" size="35"/></td></tr>
-            <tr><td><label for="resume">Résumé : </label></td><td><input type="text" name="resume" id="resume" size="35"/></td></tr>
-            <tr><td><label for="nbr_vie"> Nombre de vies en début d'aventure : </label></td><td><input type="number" name="nbr_vie" id="nbr_vie" /></td></tr>
-</table>
-Première partie
-<table>
-            <tr><td><label for="premiere_page">Première page : </label></td><td><textarea cols='' rows='' name="premiere_page" id="premiere_page"></textarea></td></tr>
-            <tr><td></td><td>Choix 1 :<select name="liaison1" id="liaison1"><option value="1">1</option> 
+            <label for="premiere_page"> <h3>Votre paragraphe</h3> </label><br/><textarea cols='90' rows='20' name="premiere_page" id="premiere_page"></textarea><br/>
+            Choix 1 :<select name="liaison1" id="liaison1"><option value="1">1</option> 
             <option value="2">2</option></select>
-            Choix 2 :<select name="liaison2" id="liaison2"><option value="1">1</option><option value="2">2</option></select></td></tr>
-</table>
-
-<table>
-            
-            <tr><td>Paragraphe 1 : </td></tr>
-            <tr><td><label for="paragraphe1">Votre paragraphe : </label></td><td><textarea cols='' rows='' name="paragraphe1" id="paragraphe1"></textarea></td></tr>
-</tr>
-</table>
+            Choix 2 :<select name="liaison2" id="liaison2"><option value="1">1</option><option value="2">2</option></select><br/>
+            <input type="submit" value="Ajouter un paragraphe" formaction="ajout_histoire.php"/>
+            <input type="submit" value="Finir mon histoire" formaction="ajout_histoire.php"/>
+</form>
     </div>
 </body>
 </html>
--- ajout_histoire_titre.php
+++ ajout_histoire_titre.php
@@ -6,14 +6,16 @@
     <div class="container">
         <?php require_once "includes/header.php"; ?>
         <form action="">
-            
+        <h3>Informations générales sur votre histoire</h3>
             <table>
-                <tr><td>Informations générales </td></tr>
+
                 <tr>
             <td><label for="titre">Titre : </label></td><td><input type="text" name="titre" id="titre" size="35"/></td></tr>
-            <tr><td><label for="resume">Résumé : </label></td><td><input type="text" name="resume" id="resume" size="35"/></td></tr>
+            <tr><td><label for="resume">Résumé : </label></td><td><br/><textarea cols='50' rows='7' name="premiere_page" id="premiere_page"></textarea><br/></td></tr>
             <tr><td><label for="nbr_vie"> Nombre de vies en début d'aventure : </label></td><td><input type="number" name="nbr_vie" id="nbr_vie" /></td></tr>
 </table>
-<input type="submit" value="Ajouter un paragraphe"/>
+<input type="submit" value="Ajouter un paragraphe" formaction="ajout_histoire.php"/>
+
+</form>
 </body>
 </html>
