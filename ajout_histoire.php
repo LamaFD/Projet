9612e824c92af463dfd37cea10511dcd9982de46
MarieLeblanc002 <101551141+MarieLeblanc002@users.noreply.github.com>
2022-04-22 11:34:49 +0200

    Ajout page histoire_creee

--- ajout_histoire.php
+++ ajout_histoire.php
@@ -7,12 +7,13 @@
         <?php require_once "includes/header.php"; ?>
         
         <form action="">
-            <label for="premiere_page"> <h3>Votre paragraphe</h3> </label><br/><textarea cols='90' rows='20' name="premiere_page" id="premiere_page"></textarea><br/>
+            <label for="titre_paragraphe"> Titre de votre paragraphe : </label><br/><input type="text" name="titre_paragraphe" id="titre_paragraphe" size="35"/></br>
+            <label for="premiere_page"> Votre paragraphe : </label><br/><textarea cols='90' rows='20' name="premiere_page" id="premiere_page"></textarea><br/>
             Choix 1 :<select name="liaison1" id="liaison1"><option value="1">1</option> 
             <option value="2">2</option></select>
             Choix 2 :<select name="liaison2" id="liaison2"><option value="1">1</option><option value="2">2</option></select><br/>
             <input type="submit" value="Ajouter un paragraphe" formaction="ajout_histoire.php"/>
-            <input type="submit" value="Finir mon histoire" formaction="ajout_histoire.php"/>
+            <input type="submit" value="Finir mon histoire" formaction="histoire_creee.php"/>
 </form>
     </div>
 </body>
---
+++ histoire_creee.php
@@ -0,0 +1,8 @@
+<!doctype html>
+<html>
+<?php require_once "includes/head.php"; ?>
+<body>
+    <h3>Félicitation ! L'histoire est créée !</h3>
+    <a href="accueil.php">Retourner à l'accueil</a>
+</body>
+</html>
