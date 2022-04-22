768b869c80f9872437bc3b7da4f4654c6b316380
LamaFD <101557128+LamaFD@users.noreply.github.com>
2022-04-22 09:12:54 +0200

    update login.php

--- login.php
+++ login.php
@@ -4,12 +4,23 @@ require("connect_bdd.php");
 session_start();
 ?>
 
+<!doctype html>
+<html>
+
+<?php require_once "includes/head.php"; ?>
+<?php require_once "includes/header.php"; ?>
 <h2 class="text-center">Connexion</h2>
     <main class="well">
-        <form method="POST">
+        <form class="text-center" method="POST">
         <input type="text" name="login" id="login" placeholder="Entrez votre login"></br>
         <input type="password" name="mdp" id="mdp" placeholder="Entrez votre mot de passe"></br>
+        Etes vous un admin ?<br/>
+        <input type="radio" name="AdminOui" id="AdminOui">
+        <label for="AdminOui">Oui</label><br/>
+        <input type="radio" name="AdminNon" id="AdminNon">
+        <label for="AdminNon">Non</label><br/>
         <button type="submit">Se connecter</button>
+        <button type="reset">Annuler</button>
         </form>
     </main>
 
@@ -35,4 +46,5 @@ session_start();
             }   
         }
     ?>
+    </html>
 
