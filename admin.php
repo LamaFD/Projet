80138e3d9e3bc8afc1d4a6104ff63c7b2b1f7526
LamaFD <101557128+LamaFD@users.noreply.github.com>
2022-05-02 23:34:18 +0200

    faire marcher page admin

--- admin.php
+++ admin.php
@@ -31,9 +31,10 @@ session_start();
             <p>Nombre de vies au debut du jeu : <?=$tuple["nbr_vie"]?> </p>
             <p>Nombre de fois joué : <?=$tuple["nbr_joue"]?> </p>
             <p> Taux de réussite : <?=($tuple["nbr_reussites"]/$tuple["nbr_vie"])*100?> </p>
-            <input type="submit" name="lire" value="Lire" formaction=<?="histoire_enCours.php?id_page=".$tuple["id_premiere_page"]?>>
+            <form method="POST">
+            <input type="submit" name="lire" value="Lire" formaction=<?="histoire_enCours.php?id_page=".$tuple["id_premiere_page"]."&vie=".$tuple["nbr_vie"]?>>
             <input type="submit" name="modif" value="Modifier" formaction=<?="modif_histoire.php?id_histoire=".$tuple["id_histoire"]?>>
-
+            </form>
         <?php
         }
         ?>
--- modif_histoire.php
+++ modif_histoire.php
@@ -17,11 +17,12 @@ session_start();
             $maRequete_histoire = "SELECT * FROM histoire WHERE id_histoire=?";
             $curseur = $BDD->prepare($maRequete_histoire);
             $curseur->execute(array($id_histoire));
+            while($tuple = $curseur->fetch()) {
 
-            $Titre = $curseur["titre"];
-            $Resume = $curseur["resume_histoire"];
-            $nbr_vies = $cursuer["nbr_vie"];
-
+            $Titre = $tuple["titre"];
+            $Resume = $tuple["resume_histoire"];
+            $nbr_vies = $tuple["nbr_vie"];
+            }
             // on cherche les pages (preparation d'affichage des pages)
             $maRequete_pages = "SELECT * FROM `page` WHERE id_histoire=? ORDER BY id_page";
             $curseur2 = $BDD->prepare($maRequete_pages);
@@ -32,23 +33,16 @@ session_start();
     ?>
 
 <body>
-
+    <main class="container">
         <form method="POST">
         <h3>Informations générales sur votre histoire</h3>
         <table>
-            <tr><td><label for="titre">Titre : </label></td><td><input type="text" name="titre" id="titre" size="35" value=<?php $Titre ?>/></td></tr>
-            <tr><td><label for="resume">Résumé : </label></td><td><br/><textarea cols='50' rows='7' name="resume" id="resume" value=<?php $Resume ?>></textarea><br/></td></tr>
-            <tr><td><label for="nbr_vie"> Nombre de vies en début d'aventure : </label></td><td><input type="number" name="nbr_vie" id="nbr_vie" value=<?php $nbr_vies ?>/></td></tr>
+            <tr><td><label for="titre">Titre : </label></td><td><input type="text" name="titre" id="titre" size="35" value=<?= $Titre ?>></td></tr>
+            <tr><td><label for="resume">Résumé : </label></td><td><br/><textarea cols='50' rows='7' name="resume" id="resume" value=<?= $Resume ?>></textarea><br/></td></tr>
+            <tr><td><label for="nbr_vie"> Nombre de vies en début d'aventure : </label></td><td><input type="number" name="nbr_vie" id="nbr_vie" value=<?= $nbr_vies ?>/></td></tr>
             
-
-
-        <?php ?>
-            <label for="titre_page"> Titre de votre page : </label><br/><input type="text" name="titre_page" id="titre_page" size="35" require/></br>
-            <label for="modif_vie"> Modification du vie : </label><br/><input type="number" name="modif_vie" id="modif_vie" size="35" require/></br>
-            <label for="page"> Votre paragraphe : </label><br/><textarea cols='90' rows='20' name="page" id="page" require></textarea><br/>
-            <input type="radio" name="Premier_page" id="Premier_page">
-            <label for="Premier_page">Est la premiere page</label><br/>
         </table>
+    </main>
 
 
 
